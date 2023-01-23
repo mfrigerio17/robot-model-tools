5a241d99983bda72f591a137529d79c80f799f47
Marco F <marco@i-r-s.ch>
2023-01-23 15:59:46 +0100

    Generate dummy joints/links to represent user frames [urdf exp]

--- src/robmodel/convert/urdf/exp.py
+++ src/robmodel/convert/urdf/exp.py
@@ -1,9 +1,13 @@
+import logging
 from mako.template import Template
 
 import robmodel.convert.utils as utils
+import robmodel.geometry
 
 import kgprim.ct as ct
 import kgprim.ct.repr.mxrepr as mxrepr
+
+logger = logging.getLogger(__name__)
 
 tpl = Template('''
 <robot name="${robot.name}">
@@ -11,6 +15,20 @@ tpl = Template('''
     <link name="${link.name}">
     </link>
 %endfor
+
+%if geometry is not None :
+%for aframe in geometry.framesModel.userAttachedFrames:
+    <link name="${aframe.entity.name}">
+    </link>
+    <% x,y,z,rx,ry,rz = dummyJointParams(geometry, aframe) %>
+    <joint name="dummy_${aframe.entity.name}" type="fixed">
+        <origin xyz="${tostr(x)} ${tostr(y)} ${tostr(z)}" rpy="${tostr(rx)} ${tostr(ry)} ${tostr(rz)}"/>
+        <parent link="${aframe.body.name}"/>
+        <child  link="${aframe.entity.name}"/>
+    </joint>
+
+%endfor
+%endif
 
 %for joint in robot.joints.values():
     <joint name="${joint.name}" type="${jointKind(joint)}">
@@ -34,14 +52,24 @@ def jointKind(joint):
         return joint.kind
     return joint.kind.name
 
-def jointOrigin(geometryModel, joint):
-    poseSpec = geometryModel.byJoint[ joint ]
+def _poseSpecToURDFJointParamters(poseSpec):
     xt = ct.frommotions.toCoordinateTransform( poseSpec )
     H  = mxrepr.hCoordinatesNumeric.matrix_repr(xt)
     irx,iry,irz = utils.getIntrinsicXYZFromR( H )
     erx,ery,erz = utils.intrinsic2extrinsic_XYZ(irx, iry, irz)
 
     return H[0,3], H[1,3], H[2,3], erx, ery, erz
+
+def fixedDummyJointOrigin(geometryModel, attachedFrame):
+    poseSpec = robmodel.geometry.getPoseSpec(geometryModel, attachedFrame)
+    if poseSpec is None:
+        logger.warning("Could not retrieve the pose of frame '{}'".format(attachedFrame.entity.name))
+        return 0,0,0,0,0,0
+    return _poseSpecToURDFJointParamters(poseSpec)
+
+def jointOrigin(geometryModel, joint):
+    poseSpec = geometryModel.byJoint[ joint ]
+    return _poseSpecToURDFJointParamters(poseSpec)
 
 
 def ordering(orderingModel):
@@ -59,6 +87,7 @@ def geometry(geometryModel):
         robot=geometryModel.connectivityModel,
         geometry=geometryModel,
         jointParams=jointOrigin,
+        dummyJointParams=fixedDummyJointOrigin,
         jointKind = jointKind,
         tostr=lambda num: formatter.float2str(num)
     )
