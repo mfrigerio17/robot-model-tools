5a553e6b17553d2a77bced8c42cf14b99c7ff0af
Marco F <marco@i-r-s.ch>
2023-12-15 17:35:22 +0100

    Add one more option for additional frames [urdf exp]

--- src/robmodel/convert/urdf/exp.py
+++ src/robmodel/convert/urdf/exp.py
@@ -9,6 +9,15 @@ import kgprim.ct.repr.mxrepr as mxrepr
 
 logger = logging.getLogger(__name__)
 
+# The template for URDF generation.
+# - generate an element for each link and joint of the input model
+# - if the geometry model is not None, also generate the joint "origin" element
+#   with its numerical attributes
+# - generate dummy link/joint pairs to represent possible additional user frames
+#   available in the input model
+# - generate dummy link/joint pairs to represent possible extra frames given to
+#   the generate function (see variable `extraPoses`)
+
 tpl = Template('''
 <robot name="${robot.name}">
 %for link in robot.links.values():
@@ -17,10 +26,11 @@ tpl = Template('''
 %endfor
 
 %if geometry is not None :
+<% dummyLinks = set() %>
 %for aframe in geometry.framesModel.userAttachedFrames:
     <link name="${aframe.entity.name}">
     </link>
-<% x,y,z,rx,ry,rz = dummyJointParams(geometry, aframe) %>
+<% x,y,z,rx,ry,rz = jointParams(geometryModel=geometry, attachedFrame=aframe);  dummyLinks.add(aframe.entity.name) %>
     <joint name="dummy_${aframe.entity.name}" type="fixed">
         <origin xyz="${tostr(x)} ${tostr(y)} ${tostr(z)}" rpy="${tostr(rx)} ${tostr(ry)} ${tostr(rz)}"/>
         <parent link="${aframe.body.name}"/>
@@ -28,12 +38,35 @@ tpl = Template('''
     </joint>
 
 %endfor
+%if extraPoses is not None :
+    <!-- Dummy links/joints to model the given extra frames -->
+
+%for pose in extraPoses:
+<%  ref = pose.pose.reference.name; tgt=pose.pose.target.name %>
+%   if ref not in robot.links.values() and ref not in dummyLinks :
+    <link name="${ref}"></link>
+<%      dummyLinks.add(ref) %>
+%   endif
+%   if tgt not in robot.links.values() and tgt not in dummyLinks :
+    <link name="${tgt}"></link>
+<%      dummyLinks.add(tgt) %>
+    %endif
+<% x,y,z,rx,ry,rz = jointParams(poseSpec=pose) %>
+    <joint name="dummy_between_${ref}_and_${tgt}" type="fixed">
+        <origin xyz="${tostr(x)} ${tostr(y)} ${tostr(z)}" rpy="${tostr(rx)} ${tostr(ry)} ${tostr(rz)}"/>
+        <parent link="${ref}"/>
+        <child  link="${tgt}"/>
+    </joint>
+%endfor
+
+    <!-- end of section for extra dummy links/joints -->
+%endif
 %endif
 
 %for joint in robot.joints.values():
     <joint name="${joint.name}" type="${jointKind(joint)}">
 %if geometry is not None :
-<% x,y,z,rx,ry,rz = jointParams(geometry, joint) %>
+<% x,y,z,rx,ry,rz = jointParams(geometryModel=geometry, joint=joint) %>
         <origin xyz="${tostr(x)} ${tostr(y)} ${tostr(z)}" rpy="${tostr(rx)} ${tostr(ry)} ${tostr(rz)}"/>
 <% x,y,z = geometry.jointAxes[joint.name] %>
         <axis xyz="${tostr(x)} ${tostr(y)} ${tostr(z)}"/>
@@ -61,15 +94,31 @@ def _poseSpecToURDFJointParamters(poseSpec):
 
     return H[0,3], H[1,3], H[2,3], erx, ery, erz
 
-def fixedDummyJointOrigin(geometryModel, attachedFrame):
-    poseSpec = robmodel.geometry.getPoseSpec(geometryModel, attachedFrame)
-    if poseSpec is None:
-        logger.warning("Could not retrieve the pose of frame '{}'".format(attachedFrame.entity.name))
-        return 0,0,0,0,0,0
-    return _poseSpecToURDFJointParamters(poseSpec)
 
-def jointOrigin(geometryModel, joint):
-    poseSpec = geometryModel.byJoint[ joint ]
+def jointOrigin(**kwargs):
+    '''
+    The six numerical parameters for a <joint> element in the URDF.
+    Accepts three combinations of arguments: geometry model and joint,
+    geometry model and attached frame, or an explicit pose-specification.
+    '''
+    poseSpec = None
+    if "geometryModel" in kwargs:
+        geometry = kwargs["geometryModel"]
+        if "joint" in kwargs:
+            poseSpec = geometry.byJoint[ kwargs["joint"] ]
+        elif "attachedFrame" in kwargs:
+            attachedFrame = kwargs["attachedFrame"]
+            poseSpec = robmodel.geometry.getPoseSpec(geometry, attachedFrame)
+            if poseSpec is None:
+                logger.warning("Could not retrieve the pose of frame '{}'".format(attachedFrame.entity.name))
+                return 0,0,0,0,0,0
+        else:
+            raise TypeError("need 'joint' or 'attachedFrame' when the geometry model is given")
+    elif "poseSpec" in kwargs:
+        poseSpec = kwargs["poseSpec"]
+    else:
+        raise TypeError("invalid arguments")
+
     return _poseSpecToURDFJointParamters(poseSpec)
 
 
@@ -84,14 +133,14 @@ def ordering(orderingModel):
         tostr=lambda num: formatter.float2str(num)
     )
 
-def geometry(geometryModel):
+def geometry(geometryModel, userExtraPoses=None):
     logger.warn("Generated joint limits are arbitrary")
     formatter = utils.FloatsFormatter()
     return tpl.render(
         robot=geometryModel.connectivityModel,
         geometry=geometryModel,
+        extraPoses=userExtraPoses,
         jointParams=jointOrigin,
-        dummyJointParams=fixedDummyJointOrigin,
         jointKind = jointKind,
         tostr=lambda num: formatter.float2str(num)
     )
