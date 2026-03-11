mfrigerio17/robot-model-tools

commit 05d3fbad51b262c0da8a9954a64010da4e758bfd

Add support for joint limits information
The idea is to be able to compose joint-limits data
with an input model format that does not account for
it (e.g. KinDSL), to export to a model format that
does (e.g. URDF).

Added new dataclass and model class to represent
joint limits, and command line option to specify
an extra input file with joint limits.

URDF export was modified to take this new model
into account, when available.

I also added a new Python module to group the
functions to load input models, to move them
out of the rmt module which is a bit too messy.
So far only loading of joint limits, though.

src/rmt/load.py
@@ -1,0 +1,30 @@
+import pathlib
+import yaml
+import json
+
+from robmodel.jlimits import JointLimits
+
+
+def jointLimits(filePath, robotConnectivityModel):
+    limits = {}
+    fpath  = pathlib.Path(filePath)
+    ext    = fpath.suffix
+    if ext == '.yaml' :
+        import yaml
+        with open(fpath) as istream:
+            limits = yaml.safe_load(istream)
+
+    elif ext == '.json' :
+        import json
+        with open(fpath) as istream:
+            limits = json.load(istream)
+
+    else:
+        log.error("Unknown extension '{}' for the joint limits file".format(ext))
+        return None
+
+    if limits["robot"] != robotConnectivityModel.name:
+        log.warning("Robot model name mismatch: '{}' (connectivity) vs '{}' (joint limits)"
+            .format(robotConnectivityModel.name, limits["robot"]))
+
+    return JointLimits(robotConnectivityModel, limits["limits"])

sample/models/ur5/ur5-joint-limits.yaml
@@ -1,0 +1,31 @@
+robot: ur5
+
+# note these limits are arbitrary, they do not reflect the actual robot
+
+limits:
+    shoulder_pan:
+        lower_pos: -0.1
+        upper_pos: 0.1
+        velocity: 0
+        force: 0
+
+    shoulder_lift:
+        lower_pos: -0.1
+        upper_pos: 0.1
+        # velocity and force are optional
+
+    elbow:
+        lower_pos: -0.1
+        upper_pos: 0.1
+
+    wr1:
+        lower_pos: -0.1
+        upper_pos: 0.1
+
+    wr2:
+        lower_pos: -0.1
+        upper_pos: 0.1
+
+    wr3:
+        lower_pos: -0.1
+        upper_pos: 0.1

sample/models/ur5/ur5.yaml
@@ -4,3 +4,4 @@ connectivity: "./ur5-connect.yaml"
 numbering:    "./ur5-numbering.yaml"
 geometry:     "./ur5-geometry.yaml"
 inertia:      "./ur5-inertia.yaml"
+joint_limits: "./ur5-joint-limits.yaml"

src/rmt/rmt.py
@@ -7,19 +7,22 @@ import robmodel.convert.urdf.imp as urdfin
 
 import robmodel.inertia
 import robmodel.jposes
+import robmodel.jlimits
 
+import rmt.load
 import rmt.kinematics
 import kgprim.values
 
 log = logging.getLogger() # get the root logger, this is a command line app
 
 
-def getmodels(filepath, paramsfilepath=None, floatLiteralsAsConstants=False):
+def getmodels(filepath, paramsFilePath=None, jlimsFilePath=None, floatLiteralsAsConstants=False):
     connectivity = None
     ordering     = None
     frames       = None
     geometry     = None
     inertia      = None
+    jlimits      = None
     _, ext = os.path.splitext(filepath)
     if ext == '.urdf' :
         log.debug("URDF format detected for file " + filepath)
@@ -84,14 +87,16 @@ def getmodels(filepath, paramsfilepath=None, floatLiteralsAsConstants=False):
                 if inertia_data == None :
                     raise RuntimeError("Could not load inertia data")
                 inertia = robmodel.inertia.RobotLinksInertia(connectivity, frames, inertia_data)
-
+        if 'joint_limits' in data:
+            path = os.path.join(basepath, data['joint_limits'])
+            jlimits = rmt.load.jointLimits(path, connectivity)
     else :
         log.error("Unknown robot model extension '{0}'".format(ext))
         exit(-1)
 
     params = {}
-    if paramsfilepath is not None:
-        fpath = pathlib.Path(paramsfilepath)
+    if paramsFilePath is not None:
+        fpath = pathlib.Path(paramsFilePath)
         ext = fpath.suffix
         if ext == '.yaml' :
             import yaml
@@ -104,7 +109,10 @@ def getmodels(filepath, paramsfilepath=None, floatLiteralsAsConstants=False):
         else:
             log.warning("Unknown extension '{}' for the parameters file".format(ext))
 
-    return connectivity, ordering, frames, geometry, inertia, params
+    if jlimsFilePath is not None:
+        jlimits = rmt.load.jointLimits(jlimsFilePath, connectivity)
+
+    return connectivity, ordering, frames, geometry, inertia, params, jlimits
 
 def defpose(args):
     robot,frames,geometry,_,paramsValues = getmodels(args.robot, args.params)[1:6]
@@ -117,7 +125,7 @@ def defpose(args):
     print(np.round(H,5))
 
 def printinfo(args):
-    c,o,f,g,i,_ = getmodels(args.robot)
+    c,o,f,g,i = getmodels(args.robot)[0:5]
     print(c,o,f,g,i)
 
 def writeDOTFile(args):
@@ -168,7 +176,9 @@ def _resolve_parameters(poseSpecIterable, parametersValues):
                         count = count + 1
 
 def export(args):
-    c,o,f,g,i,params = getmodels(args.robot, args.params)[0:6]
+    c,o,f,g,i,params,jlimits = getmodels(args.robot,
+                                paramsFilePath = args.params,
+                                jlimsFilePath  = args.jlims)[0:7]
     oformat = args.oformat
 
     if oformat is None: oformat = 'yaml'
@@ -200,7 +210,7 @@ def export(args):
                     poseSpecModel = motdsl.toPosesSpecification(model)
                     _resolve_parameters(poseSpecModel.poses, params)
                     extraPoses = poseSpecModel.poses
-                text = urdfout.modelText(g, i, userExtraPoses=extraPoses)
+                text = urdfout.modelText(g, i, jointLimits=jlimits, userExtraPoses=extraPoses)
             elif o is None:
                 log.error("Cannot export a URDF if the input model does not even include ordering")
                 exit(-1)
@@ -228,7 +238,10 @@ def export(args):
         # do not close() sys.stdout :)
 
 def playground(args):
-    c,o,f,geometry,inertia,params = getmodels(args.robot, args.params, True)[0:6]
+    c,o,f,geometry,inertia,params,jlimits = getmodels(args.robot,
+                                        paramsFilePath = args.params,
+                                        jlimsFilePath  = args.jlims,
+                                        floatLiteralsAsConstants=True)[0:7]
     for link in c.links.values() :
         ip = inertia.byLink(link)
         if ip is not None:
@@ -238,10 +251,14 @@ def playground(args):
         for m in pose.motion.steps:
             print(m)
 
+    for name,limits in jlimits.byJoint.items():
+        print(name,limits)
+
 
 def setRobotArgs(argparser):
     argparser.add_argument('robot', metavar='robot-model', help='the robot model input file')
     argparser.add_argument('-p', '--params', dest='params', metavar='params-file', default=None, help='YAML/JSON file with default parameter values')
+    argparser.add_argument('-j', '--joint-limits', dest='jlims', metavar='jlims-file', default=None, help='YAML/JSON file with joint limits data')
 
 def main():
     formatter = logging.Formatter('%(levelname)s (%(name)s) : %(message)s')

src/robmodel/convert/urdf/exp.py
@@ -1,9 +1,11 @@
 import logging
 import numpy as np
+import dataclasses
 from mako.template import Template
 
 import robmodel.convert.utils as utils
 import robmodel.geometry
+import robmodel.jlimits
 
 import kgprim.ct as ct
 import kgprim.ct.repr.mxrepr as mxrepr
@@ -79,6 +81,7 @@ tpl = Template('''
 
 %for joint in robot.joints.values():
     <joint name="${joint.name}" type="${jointKind(joint)}">
+<% jlim = jlimits.byJoint[joint] %>
 %if geometry is not None :
 <% x,y,z,rx,ry,rz = jointParams(geometryModel=geometry, joint=joint) %>
         <origin xyz="${tostr(x)} ${tostr(y)} ${tostr(z)}" rpy="${tostr(rx)} ${tostr(ry)} ${tostr(rz)}"/>
@@ -87,7 +90,7 @@ tpl = Template('''
 %endif
         <parent link="${robot.predecessor(joint).name}"/>
         <child  link="${robot.successor  (joint).name}"/>
-        <limit effort="30" velocity="10.0" lower="-3.14" upper="3.14" />
+        <limit effort="${jlim.force}" velocity="${jlim.velocity}" lower="${jlim.lower_pos}" upper="${jlim.upper_pos}" />
     </joint>
 
 % endfor
@@ -197,13 +200,19 @@ def ordering(orderingModel):
         tostr=lambda num: formatter.float2str(num)
     )
 
-def modelText(geometryModel, inertiaModel=None, userExtraPoses=None):
-    logger.warn("Generated joint limits are arbitrary")
+def modelText(geometryModel, inertiaModel=None, userExtraPoses=None, jointLimits=None):
+    if jointLimits is None:
+        logger.warn("generated joint limits are arbitrary")
+        limits = {joint : dataclasses.asdict(robmodel.jlimits.JointLimit())
+                     for joint in geometryModel.connectivityModel.joints.keys()}
+        jointLimits = robmodel.jlimits.JointLimits(geometryModel.connectivityModel, limits)
+
     formatter = utils.FloatsFormatter()
     return tpl.render(
         robot=geometryModel.connectivityModel,
         geometry=geometryModel,
         inertia=inertiaModel,
+        jlimits=jointLimits,
         extraPoses=userExtraPoses,
         jointParams=jointOrigin,
         jointKind = jointKind,

src/robmodel/convert/yaml/imp.py
@@ -101,6 +101,9 @@ def geometry(istream, floatLiteralsAsConstants=False) :
     return PosesSpec(**out)
 
 
+def joint_limits(istream):
+    return yaml.safe_load(istream)
+
 
 def _getPropertyValue(yamlvalue, floatLiteralsAsConstants=False):
     ret = yamlvalue

src/robmodel/jlimits.py
@@ -1,0 +1,32 @@
+from dataclasses import dataclass
+
+@dataclass
+class JointLimit:
+    lower_pos : float = 0
+    upper_pos : float = 0
+    velocity  : float = 0
+    force     : float = 0
+
+
+class JointLimits:
+    def __init__(self, connectivity_model, ranges_by_name):
+        '''
+        - `ranges_by_name` dictionary of dictionaries: the first must be keyed
+           by joint name, and every value must be unpackable to a `JointLimit`
+           instance, i.e., it must be possible to do
+           `JointLimit(**ranges_by_name[someJointName]) `
+        '''
+        self.robot = connectivity_model
+        self.limits = {}
+
+        for jname in ranges_by_name:
+            if jname not in self.robot.joints:
+                raise RuntimeError("cannot find joint '{}' in robot {}".format(jname, self.robot.name))
+
+            joint  = self.robot.joints[jname]
+            limits = ranges_by_name[jname]
+            self.limits[joint] = JointLimit( **limits )
+
+    @property
+    def byJoint(self):
+        return self.limits

commit bb91f99a546463f377b6d1fc329a122fba162c2f

Fix and consolidate the load module [bugfix]
Bug and defect introduced in 05d3fbad51b:

  - using a logger without creating/defining it

  - importing json and yaml while also conditionally
    importing them in the function (which is what I want)

Introduce a generic reusable function to load yaml/json

src/rmt/load.py
@@ -1,30 +1,40 @@
 import pathlib
-import yaml
-import json
-
 from robmodel.jlimits import JointLimits
 
 
-def jointLimits(filePath, robotConnectivityModel):
-    limits = {}
-    fpath  = pathlib.Path(filePath)
-    ext    = fpath.suffix
+def loadDictionary(filePath):
+    '''
+    Load the dictionary encoded in the given YAML/JSON file.
+    The empty dictionary is returned in case of any error.
+    '''
+    if filePath is None :
+        return {}
+    data = {}
+    fpath = pathlib.Path(filePath)
+    ext = fpath.suffix
     if ext == '.yaml' :
         import yaml
         with open(fpath) as istream:
-            limits = yaml.safe_load(istream)
-
+            data = yaml.safe_load(istream)
     elif ext == '.json' :
         import json
         with open(fpath) as istream:
-            limits = json.load(istream)
-
+            data = json.load(istream)
     else:
-        log.error("Unknown extension '{}' for the joint limits file".format(ext))
+        logger.error("Unknown extension '{}' for the dictionary file".format(ext))
+    return data
+
+
+def jointLimits(filePath, robotConnectivityModel):
+    limits = loadDictionary(filePath)
+    if len(limits) == 0:
         return None
 
     if limits["robot"] != robotConnectivityModel.name:
-        log.warning("Robot model name mismatch: '{}' (connectivity) vs '{}' (joint limits)"
+        logger.warning("Robot model name mismatch: '{}' (connectivity) vs '{}' (joint limits)"
             .format(robotConnectivityModel.name, limits["robot"]))
 
     return JointLimits(robotConnectivityModel, limits["limits"])
+
+
+
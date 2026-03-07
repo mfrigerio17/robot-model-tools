bb91f99a546463f377b6d1fc329a122fba162c2f
Marco F <3646506+mfrigerio17@users.noreply.github.com>
2026-03-07 17:25:21 +0100

    Fix and consolidate the load module [bugfix]
    Bug and defect introduced in 05d3fbad51b:
    
      - using a logger without creating/defining it
    
      - importing json and yaml while also conditionally
        importing them in the function (which is what I want)
    
    Introduce a generic reusable function to load yaml/json

--- src/rmt/load.py
+++ src/rmt/load.py
@@ -1,30 +1,40 @@
 import pathlib
-import yaml
-import json
-
 from robmodel.jlimits import JointLimits
 
 
-def jointLimits(filePath, robotConnectivityModel):
-    limits = {}
-    fpath  = pathlib.Path(filePath)
-    ext    = fpath.suffix
+def loadDictionary(filePath):
+    '''
+    Load the dictionary encoded in the given YAML/JSON file.
+    The empty dictionary is returned in case of any error.
+    '''
+    if filePath is None :
+        return {}
+    data = {}
+    fpath = pathlib.Path(filePath)
+    ext = fpath.suffix
     if ext == '.yaml' :
         import yaml
         with open(fpath) as istream:
-            limits = yaml.safe_load(istream)
-
+            data = yaml.safe_load(istream)
     elif ext == '.json' :
         import json
         with open(fpath) as istream:
-            limits = json.load(istream)
-
+            data = json.load(istream)
     else:
-        log.error("Unknown extension '{}' for the joint limits file".format(ext))
+        logger.error("Unknown extension '{}' for the dictionary file".format(ext))
+    return data
+
+
+def jointLimits(filePath, robotConnectivityModel):
+    limits = loadDictionary(filePath)
+    if len(limits) == 0:
         return None
 
     if limits["robot"] != robotConnectivityModel.name:
-        log.warning("Robot model name mismatch: '{}' (connectivity) vs '{}' (joint limits)"
+        logger.warning("Robot model name mismatch: '{}' (connectivity) vs '{}' (joint limits)"
             .format(robotConnectivityModel.name, limits["robot"]))
 
     return JointLimits(robotConnectivityModel, limits["limits"])
+
+
+
